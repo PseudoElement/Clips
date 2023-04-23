a7448768634e874bad0b499cb5bd0ec6cfe49798
PseudoElement <bubamainer@mail.ru>
2023-04-24 00:41:34 +0300

    pipes/directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,16 @@ export class AppComponent {
      title = "hosting-ang";
      name = "Luis";
      imgURL = "https://picsum.photos/id/237/500/500";
+     currentDate = new Date();
+     cost = 2000;
+     temperature = 25.7;
+     pizza = {
+          toppings: ["peperoni", "bacon"],
+          size: "large",
+     };
+     blueClass = false;
+     fontSize = 46;
+     images = ["https://picsum.photos/id/237/500/500", "https://picsum.photos/id/23/500/500", "https://picsum.photos/id/29/500/500"];
      getName() {
           return this.name;
      }
