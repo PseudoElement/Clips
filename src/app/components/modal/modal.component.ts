7c45ce7753b4c99c8f35791ff3d089f1b071286b
PseudoElement <bubamainer@mail.ru>
2023-04-25 02:51:41 +0300

    add service

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AuthModalService } from "src/app/services/auth-modal.service";
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+    selector: "app-modal",
+    templateUrl: "./modal.component.html",
+    styleUrls: ["./modal.component.scss"],
 })
 export class ModalComponent {
-
+    constructor(public modalService: AuthModalService) {}
 }
---
+++ src/app/services/auth-modal.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: "root",
+})
+export class AuthModalService {
+    visible = false;
+    constructor() {}
+}
